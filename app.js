cd2c8f029016dd63a5629bde2da2d0fb68a3d216
MuhammadHaikalFadlurrohman <haikalfadlurrohman@gmail.com>
2025-09-19 08:37:42 +0700

    Baru

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.writeHead(200,{
+        'Content-Type': 'text/html'
+    });
+    res.write('Hello Word Haiiii');
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log(`Listening is Listening on Port ${port}/`);
+});
+
+
